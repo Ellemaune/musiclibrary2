ee64930ebbfea5cc70e37ef1dd52cc18775c6f1e
sahad <saha.dodonov@gmail.com>
2021-12-02 19:18:48 +0400

    Hello word!

--- src/com/netcracker/musiclibrary/controller/Controller.java
+++ src/com/netcracker/musiclibrary/controller/Controller.java
@@ -1,5 +1,8 @@
 package com.netcracker.musiclibrary.controller;
 
 public class Controller {
-    int num = 10;
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+
 }
