6f92f77a83232eec244d98aadf6d0e320240dde0
nansealy <84577731+nansealy@users.noreply.github.com>
2021-12-27 11:01:57 +0400

    Add files via upload

---
+++ src/main/java/com/netcracker/musiclibrary/data/Genre.java
@@ -0,0 +1,10 @@
+package com.netcracker.musiclibrary.data;
+
+import java.io.Serializable;
+
+/** Record Genre containing fields: name
+ * @param name - Name of Genre
+ */
+public record Genre (String name) implements Serializable {
+
+}
