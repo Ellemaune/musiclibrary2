7e7c55e0e753a57aec36da9e85e1bdef939d3636
sahad <saha.dodonov@gmail.com>
2021-12-28 21:53:15 +0400

    make mogel and add @get list of tracks or genres

--- src/main/java/com/netcracker/musiclibrary/GreetingResource.java
+++ src/main/java/com/netcracker/musiclibrary/GreetingResource.java
@@ -1,15 +1,38 @@
 package com.netcracker.musiclibrary;
 
+import com.netcracker.musiclibrary.data.Genre;
+import com.netcracker.musiclibrary.data.Track;
+import com.netcracker.musiclibrary.model.Model;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.Collection;
 
-@Path("/hello")
+@Path("/myprog")
 public class GreetingResource {
+    @Inject
+    Model model;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from the MusicLibrary!";
+    public String myprog() {
+        return "go to tracks or genres";
+    }
+
+    @GET
+    @Path("/tracks")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Collection<Track> getTracks() {
+        return model.getTracksCollection();
+    }
+
+    @GET
+    @Path("/genres")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Collection<Genre> getGenres() {
+        return model.getGenresCollection();
     }
 }
