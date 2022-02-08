1b469bc4759a034ce5a3ea6c2461421e89160a79
sahad <saha.dodonov@gmail.com>
2022-02-09 00:15:57 +0400

    test for fileResource v0.2

--- src/test/java/com/netcracker/musiclibrary/FileResourceTest.java
+++ src/test/java/com/netcracker/musiclibrary/FileResourceTest.java
@@ -1,18 +1,9 @@
 package com.netcracker.musiclibrary;
 
 import io.quarkus.test.junit.QuarkusTest;
-import io.restassured.path.json.JsonPath;
-import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectInputStream;
-
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-import io.restassured.path.json.JsonPath;
 
 @QuarkusTest
 public class FileResourceTest {
