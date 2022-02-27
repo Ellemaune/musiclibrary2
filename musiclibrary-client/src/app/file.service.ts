898ef647ac2feee1dc22ce2db2433fb9fec4715e
sahad <saha.dodonov@gmail.com>
2022-02-27 14:48:41 +0400

    connected material.angular and make some first step

---
+++ musiclibrary-client/src/app/file.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from "@angular/common/http";
+
+export class FileService{
+  constructor(private http: HttpClient) {
+  }
+
+  uploadFile(){
+
+  }
+
+  downloadFile(){
+
+  }
+}
